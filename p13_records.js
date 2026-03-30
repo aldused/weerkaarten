a9ed7c796cbe721c482478ae57d0407436633207
Ed Aldus <jouw@email.nl>
2026-03-30 09:36:21 +0200

    Kaarten update 2026-03-30 09:36

--- p13_records.js
+++ p13_records.js
@@ -19,7 +19,7 @@ const P13_DATA = {
     "periode_eind": "2026-02-28",
     "min_stations": 10,
     "top_n": 25,
-    "gegenereerd": "2026-03-29 05:20"
+    "gegenereerd": "2026-03-30 05:20"
   },
   "records": {
     "natste_dag": [
